4473d220cee31529774a6e132f30c8f834a373aa
helgamayr <67434322+helgamayr@users.noreply.github.com>
2020-12-26 10:45:51 +0100

    day1 find numbers that sum to 2020

---
+++ src/day1_2020/FindTwoNumbersThatSumTo2020.java
@@ -0,0 +1,100 @@
+package day1_2020;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class FindTwoNumbersThatSumTo2020 {
+	
+	public static void main(String[] args) {
+		
+		ArrayList<Integer> numbers = retrieveNumbers();
+		System.out.println(numbers.size());	 
+		
+		int[] twoNumbers = findTwoNumbers(numbers,2020,-1);
+		System.out.println(twoNumbers.length);	
+		
+		System.out.println(twoNumbers[0] * twoNumbers[1]);
+		
+		int[] threeNumbers = findThreeNumbers(numbers,2020);
+		
+		System.out.println(threeNumbers.length);	
+		
+		System.out.println(threeNumbers[0] * threeNumbers[1] * threeNumbers[2]);
+	}
+	
+	public static ArrayList<Integer> retrieveNumbers() {
+		
+		File numbersFile = new File("day1-2020-puzzleinput.txt");
+		ArrayList<Integer> numbers = new ArrayList<Integer>();
+		
+		try(FileReader fr = new FileReader(numbersFile);
+				BufferedReader br = new BufferedReader(fr)){
+			while(br.ready()) {
+				numbers.add(new Integer(br.readLine()));
+			}
+					
+		} catch (FileNotFoundException fne) {
+			fne.printStackTrace();
+		}catch (IOException ioe) {
+			ioe.printStackTrace();
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return numbers;
+	}
+
+	public static int[] findTwoNumbers(ArrayList<Integer> numbers, int matchSum, int exclude) {
+		
+		int numberOne;
+		int numberTwo;
+		int sum;
+		
+		for (int i = 0; i < numbers.size(); i++) {
+			
+			if (i == exclude)  { continue;}
+			
+			for (int k = i+1; k < numbers.size(); k++) {
+				
+				sum = numbers.get(i) + numbers.get(k);
+				
+				if ( sum == matchSum) {
+					return new int[] {numbers.get(i), numbers.get(k)};
+				}
+			}
+		}
+				
+		
+		return new int[] {};
+	}
+	
+	public static int[] findThreeNumbers(ArrayList<Integer> numbers, int matchSum) {
+		
+		int numberOne;
+		int sum;
+		int[] twoNumbers;
+		
+		for (int i = 0; i < numbers.size(); i++) {
+				
+				twoNumbers = findTwoNumbers(numbers, matchSum-numbers.get(i), i);
+				
+				if (twoNumbers.length != 0) {
+				
+				sum = numbers.get(i) + twoNumbers[0] + twoNumbers[1];
+				
+				if ( sum == matchSum) {
+					return new int[] {numbers.get(i), twoNumbers[0] , twoNumbers[1]};
+				}
+				}
+			
+		};
+				
+		
+		return new int[] {};
+	}
+	
+}
