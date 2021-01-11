733af3a8aea05e38ead67eba362c20d686067ac6
Helga Mayr <info@fasep.de>
2021-01-11 12:41:57 +0100

    day 10 adapter combinations

--- src/day10_2020/JoltageAdapters.java
+++ src/day10_2020/JoltageAdapters.java
@@ -9,11 +9,9 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
-import java.util.Set;
 import java.util.TreeMap;
 
 /*
